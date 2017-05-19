9f5b3edf41a2bef5c1896631cd25154c04d06793
qujie <793116483@qq.com>
2017-05-19 15:04:57 +0800

    Initial Commit

---
+++ QJWebImage/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  QJWebImage
+//
+//  Created by 瞿杰 on 2017/5/19.
+//  Copyright © 2017年 yiniu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
